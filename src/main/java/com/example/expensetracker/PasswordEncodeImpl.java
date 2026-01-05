510572e791606472e78e770e74f6379256cd87f4
mkadar <mkadar@deloitte.com>
2026-01-05 11:39:40 +0800

    add manual password encoder function

---
+++ src/main/java/com/example/expensetracker/PasswordEncodeImpl.java
@@ -0,0 +1,13 @@
+package com.example.expensetracker;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class PasswordEncodeImpl {
+
+    public static void main (String[] args){
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(passwordEncoder.encode("admin-password"));
+    }
+}
